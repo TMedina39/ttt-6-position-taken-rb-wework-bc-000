9525499e93335c8e9dcda157e70624cdc65f1879
Tomas Medina <tomas.medina@wework.com>
2018-02-11 20:12:20 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(array,index)
-  if array[index] == "X" || "O"
+  if array[index] == "X"
+    true
+  elsif array[index] == "O"
     true
   else
     false
